796abd66ee11a07e3bd9b96203812e3d25db330e
Leandro Flores <109381840+we1rdou@users.noreply.github.com>
2022-11-19 19:03:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Ingrese primer numero: ");
+        int num1=int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Ingrese segundo numero: ");
+        int num2=int.Parse(Console.ReadLine());
+        if(num1>num2)
+        {
+            System.Console.WriteLine("El numero mayor es: "+ num1);
+        }
+        else
+        {
+            System.Console.WriteLine("El numero mayor es: "+ num2);
+        }
+    }
+}
