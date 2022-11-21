d8b64a04380dfd90510aa0b01687e7fd8c83f502
Leandro Flores <109381840+we1rdou@users.noreply.github.com>
2022-11-20 21:56:26 -0500

    Add files via upload

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,11 @@
+﻿//ejercicio 8
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for(int x=3;x<=3000;x=x+3)
+        {
+            System.Console.WriteLine(x);
+        }
+    }
+}
